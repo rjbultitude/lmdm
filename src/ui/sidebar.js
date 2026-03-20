bf6c8a2698d8990549a5674da1c5a6c4b4a00771
Richard Bultitude <richard.bultitude@gmail.com>
2026-03-20 19:49:20 +0000

    implement basic show/hide for control panels

--- src/ui/sidebar.js
+++ src/ui/sidebar.js
@@ -1,8 +1,11 @@
 export function initSidebarControls() {
   const sideBars = document.getElementsByClassName("controls-sidebar");
   const ACTIVE = "active";
+  console.log("sideBars", sideBars);
   Array.from(sideBars).forEach((sideBar) => {
     sideBar.addEventListener("click", function(e) {
+      e.stopPropagation();
+      e.preventDefault();
       const thisSlideButton = e.target;
       const thisSlideBar = thisSlideButton.parentElement;
       if (thisSlideBar.classList.contains(ACTIVE)) {
